7ae02fce27117941e3e4dcea9cfeb248e19160a3
Vinayakv01 <vinayakv949@gmail.com>
2024-04-17 11:33:48 +0530

    navchange

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,9 +38,9 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        {/* <Navbar.Brand href="/" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+        </Navbar.Brand> */}
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
